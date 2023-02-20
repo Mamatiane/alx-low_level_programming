bf0bc2f736507a1a0800b007fdc1739cf6d40a5b
mamatiane <andrymamatiane@gmail.com>
2023-02-20 04:50:26 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * Return 0 (success)
+ * Discription: prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0
  */
 
 int main(void)
