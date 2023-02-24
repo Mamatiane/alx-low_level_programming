8fc4bd19915ff148a0b3e0a9765b8cf9d0a16154
mamatiane <andrymamatiane@gmail.com>
2023-02-24 15:47:34 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers - print numbers from 0 - 9
+ *
  * Return: void
  */
 
@@ -11,7 +12,7 @@ void print_numbers(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(c);
+		putchar(c);
 	}
-	_putchar('\n');
+	putchar('\n');
 }
