45fcdf2400e526c1ddd9b621f0067616ff607d28
mamatiane <andrymamatiane@gmail.com>
2023-02-23 17:55:59 -0800

    3-print_remaining_days.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -9,15 +9,61 @@
 * @year: year
 * Return: void
 */
-
-int main(void)
+void print_remaining_days(int month, int day, int year)
 {
-	int month, day, year;
-
-	printf("Enter date (MM/DD/YYYY): ");
-	scanf("%d/%d/%d", &month, &day, &year);
-	print_remaining_days(month, day, year);
+	int remaining_days = 0;
 	
-	return (0);
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+			{
+				if (month > 2)
+					remaining_days = 1;
+			}
+		}
+		else
+		{
+			if (month > 2)
+				remaining_days = 1;
+		}
+	}
+	if (month < 1 || month > 12)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+	}
+	if (day < 1 || day > 31)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+	}
+	if ((month == 4 || month == 6 || month == 9 || month == 11) && day == 31)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+		return;
+	}
+	if (month == 2)
+	{
+		if (remaining_days == 0 && day > 28)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			return;
+		}
+		if (remaining_days == 1 && day > 29)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+			return;
+		}
+	}
+	if (remaining_days == 1)
+	{
+		printf("Day of the year: %d\nRemaining days: %d\n", day + 31 + 29, 366 - day - 31 - 29);
+	}
+	else
+	{
+		printf("Day of the year: %d\nRemaining days: %d\n", day + (month - 1) * 31 - ((month - 1) / 8) * (month - 1) + remaining_days, 365 - day - (month - 1) * 31 + ((month - 1) / 8) * (month - 1) - remaining_days);
+	}
 }
 
