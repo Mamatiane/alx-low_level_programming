a3a1024e893026299fe899348a91073b51bde0b3
mamatiane <andrymamatiane@gmail.com>
2023-03-05 15:44:28 -0800

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,41 +6,18 @@
  */
 int main(void)
 {
-	int c, boolean, boolean2;
-	long int n1, n2, fn, fn2, n11, n22;
+	unsigned long int prev = 1, curr = 2, next;
+	int i;
 
-	n1 = 1;
-	n2 = 2;
-	boolean =  boolean2 = 1;
-	printf("%ld, %ld", n1, n2);
-	for (c = 0; c < 96; c++)
+	printf("%lu, %lu, ", prev, curr);
+	for (i = 3; i <= 98; i++)
 	{
-		if (boolean)
-			fn = n1 + n2;
-		printf(", %ld", fn);
-		n1 = n2;
-		n2 = fn;
-		else
-		{
-			if (boolean2)
-			{
-				n11 = n1 % 1000000000;
-				n22 = n2 % 1000000000;
-				n1 = n1 / 1000000000;
-				n2 = n2 / 1000000000;
-				boolean2 = 0;
-			}
-			fn2 = (n11 + n22);
-			fn = n1 + n2 + (fn2 / 1000000000);
-			printf(", %ld", fn);
-			printf("%ld", fn2 % 1000000000);
-			n1 = n2;
-			n11 = n22;
-			n2 = fn;
-			n22 = (fn2 % 1000000000);
-		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		next = prev + curr;
+		printf("%lu", next);
+		if (i < 98)
+			printf(", ");
+		prev = curr;
+		curr = next;
 	}
 	printf("\n");
 	return (0);
