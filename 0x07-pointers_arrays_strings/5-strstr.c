66ae4a7edaecc8792e92b7d541a72a7fe4426ca8
mamatiane <andrymamatiane@gmail.com>
2023-03-06 14:30:47 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,13 +6,13 @@
  * @needle: input
  * Return: Always 0.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *l = haystack;
 		char *p = needle;
+
 		while (*l == *p && *p != '\0')
 		{
 			l++;
