cbe364710be15aea034cf3ebefeddb34eab12809
mamatiane <andrymamatiane@gmail.com>
2023-03-16 06:59:54 -0700

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,16 +12,16 @@
  */
 int *array_range(int min, int max)
 {
-	int *ar;
-	int i;
+	int *arr;
+	int size, i;
 
 	if (min > max)
 		return (NULL);
-	ar = malloc(sizeof(*ar) * ((max - min) + 1));
-	if (ar == NULL)
+	size = max - min + 1;
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
 		return (NULL);
-	for (i = 0; min <= max; i++, min++)
-		ar[i] = min;
-	ar[i] = min;
-	return (ar);
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
+	return (arr);
 }
