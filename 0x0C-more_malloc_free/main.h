ae7c5c2d54c87ba5696a0121daf357453309b6fc
mamatiane <andrymamatiane@gmail.com>
2023-03-16 15:41:48 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,4 +16,11 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+void _print(char *str, int l);
+
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+
+int check_for_digits(char **av);
+
+void init(char *str, int l);
 #endif
