bb74beb3f0143f8bdcf1298bca2a0f4cbaf9f22e
Mamatiane <andrymamatiane@gmail.com>
2023-07-10 05:45:48 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,3 +1,5 @@
+#include "new_functions.c"
+
 #include <elf.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -5,7 +7,6 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "new_functions.c"
 
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
