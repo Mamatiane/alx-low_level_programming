8c279b9d772e28c9a22ebbdb40b9eea6635437f0
Mamatiane <andrymamatiane@example.com>
2023-08-21 17:42:01 -0700

    libgiga.so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/Mamatiane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+wget https://github.com/Mamatiane/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so -O /tmp/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
